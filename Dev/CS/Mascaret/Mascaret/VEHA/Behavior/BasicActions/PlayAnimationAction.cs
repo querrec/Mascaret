63817c034445a77926feaf9d72750cc795e77c90
Joanna TAOUM <joanna.taoum27@gmail.com>
2015-09-01 10:06:39 +0200

    Add PlayAnimation function.

---
+++ Dev/CS/Mascaret/Mascaret/VEHA/Behavior/BasicActions/PlayAnimationAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mascaret
+{
+    class PlayAnimationAction : Action, IEquatable<PlayAnimationAction>
+    {
+
+        public string animationName;
+
+        public PlayAnimationAction()
+        {
+            Kind = "PlayAnimation";
+        }
+
+        public bool Equals(PlayAnimationAction other)
+        {
+            if (other == this)
+                return true;
+            return false;
+        }
+
+        public override BehaviorExecution createBehaviorExecution(InstanceSpecification host, Dictionary<string, ValueSpecification> p, bool sync)
+        {
+            PlayAnimationBehaviorExecution behavior = new PlayAnimationBehaviorExecution(this, host, p);
+            return behavior;
+        }
+
+    }
+}
