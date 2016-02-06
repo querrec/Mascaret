1195c262f3ad9f881c247052a32d592a9bc2bea2
Joanna TAOUM <joanna.taoum27@gmail.com>
2016-02-06 10:24:34 +0100

    Add EmotionBehavior.

---
+++ Dev/CS/Mascaret/Mascaret/BEHAVE/EmotionBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Mascaret
+{
+    public class EmotionBehavior : CyclicBehaviorExecution
+
+    {
+        public EmotionBehavior(Behavior specif, InstanceSpecification host, Dictionary<String, ValueSpecification> p)
+            : base(specif, host, p)
+        {
+        }
+
+        public EmotionBehavior()
+        {
+        }
+
+        public override void action()
+        {
+            MascaretApplication.Instance.VRComponentFactory.Log("JE SUIS LE NOUVEAU COMPORTEMENT*********************JE FAIS RIEN POUR L INSTANT");
+        }
+    }
+}
